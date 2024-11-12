ac3424592b7cbf157785e28b1c04b2d7ebb4689d
Enrico Cotti Cottini <enrico.cotticottini@gmail.com>
2024-11-12 18:51:06 +0100

    Raccolta e strutturati dati prodotti

--- README.md
+++ README.md
@@ -1,18 +1,38 @@
 # Repo progetto RO
 
-## Initialisation python venv
+## Getting Data from Coop website
+Navigate to the data folder:
+```bash
+cd data
+```
+
+### Initialisation python venv
 ```bash
 python -m venv dieta
 source dieta/bin/activate
 pip install -r Requirements.txt
 ```
 
-## Run the project
-To get the Coop products from a list of links of the site  https://padova.easycoop.com , make shure there link ins ref.txt are updated (do webscraping if you need) then run the following command:
+### link sources
+The links to the products are stored in the file ref.txt. This links are the links of all the products page of the site https://padova.easycoop.com, i got this by webscraping the site catalog (https://padova.easycoop.com/catalogsearch/result?q=&page=1 from page 1 to 320) with a python script using a docker container whit fireforx drivers and selenium similar to product_getter.py. searching for links in href of tag "a" with class "nuxt-link-prefetched product-tile__link". links are stored in tag format:
+```
+<a href=" Link to product " class="nuxt-link-prefetched product-tile__link"></a>
+```
+this way i got a list of 9570 links to the products of the site.
+
+### Run the driver container
+To run the driver container you need to have docker installed on your machine, navigate to the docker-selenium dir in the root file, then run the following command:
+```bash
+docker run -d -p 4444:4444 -p 7900:7900 --shm-size="2g" selenium/standalone-firefox:4.26.0-20241101
+```
+
+### Run the data getter
+To get the Coop products from a list of links of the site  https://padova.easycoop.com , make sure there are links in ref.txt updated (do webscraping if you need) then run the following command:
 
 ```bash
 python product_getter.py
 ```
+to run this you have to run the docker container.
 
 This will get you a list of product in the file products.json in the format:
 ```json
--- product_getter.py
+++ product_getter.py
@@ -5,114 +5,146 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
+import json
 
-# Leggi gli URL da ref.txt, escludendo quelli che iniziano con #
-with open('ref.txt', 'r') as file:
-    urls = [line.strip() for line in file.readlines() if not line.startswith('#')]
+def scrape_products(urls, driver, output_file, links_file):
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write('[')
+        
+    for url in urls:
+        start_time = time.time()
 
-# Imposta le opzioni di Firefox per la modalità headless
-options = Options()
-options.headless = True
+        driver.get(url)
 
-# Inizializza WebDriver per connettersi al contenitore Selenium
-driver = webdriver.Remote(
-    command_executor='http://localhost:4444/wd/hub',
-    options=options
-)
+        try:
+            WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "product-title__brand"))
+            )
+        except Exception as e:
+            print(f"Errore nel caricamento dei prodotti da {url}: {e}")
+            with open(links_file, 'r') as file:
+                lines = file.readlines()
+            with open(links_file, 'w') as file:
+                for line in lines:
+                    if line.strip() == url:
+                        file.write(f"#{line}")
+                    else:
+                        file.write(line)
+            continue
 
-for url in urls:
-    # Inizia il timing
-    start_time = time.time()
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    # Apri l'URL
-    driver.get(url)
+        load_page_time = time.time()
+        print(f"Time to load page and parse HTML from {url}: {load_page_time - start_time:.2f} seconds")
 
-    # Attendi fino a quando gli elementi del prodotto sono presenti
-    try:
-        # Attendi la presenza del contenitore principale che contiene i prodotti
-        WebDriverWait(driver, 3).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "product-title__brand"))
-        )
-    except Exception as e:
-        print(f"Errore nel caricamento dei prodotti da {url}: {e}")
-        with open('ref.txt', 'r') as file:
+        brand_element = soup.find('h5', class_='product-title__brand')
+        brand = brand_element.text.strip() if brand_element else 'Unknown'
+
+        name_element = soup.find('h4', class_='product-title__name')
+        name = name_element.text.strip() if name_element else 'Unknown'
+
+        price_per_qty_element = soup.find('span', class_='product__price-per-qty')
+        price_per_qty = price_per_qty_element.text.strip() if price_per_qty_element else 'Unknown'
+
+        price_element = soup.find('span', class_='price__final')
+        price = price_element.text.strip() if price_element else 'Unknown'
+
+        nutritional_values = {}
+        nutrition_table = soup.find('table', class_='product-information-table')
+
+        if nutrition_table:
+            headers = [header.text.strip() for header in nutrition_table.select('thead th')]
+            rows = nutrition_table.select('tbody tr')
+            for row in rows:
+                columns = row.select('td')
+                if len(columns) >= len(headers):
+                    nutrient = columns[0].text.strip()
+                    values = {headers[i]: columns[i].text.strip() for i in range(1, len(headers))}
+                    nutritional_values[nutrient] = values
+
+        print(f"Brand: {brand}")
+        print(f"Name: {name}")
+        print(f"Price per quantity: {price_per_qty}")
+        print(f"Price: {price}")
+        print(f"Nutritional values : {nutritional_values}")
+        print(f"Url: {url}")
+
+        product_data = {
+            "Brand": brand,
+            "Name": name,
+            "Price per quantity": price_per_qty,
+            "Price": price,
+            "Nutritional values": nutritional_values,
+            "Url": url
+        }
+
+        with open(output_file, 'a', encoding='utf-8') as file:
+            json.dump(product_data, file, ensure_ascii=False, indent=4)
+            file.write(',')
+            file.write('\n')
+
+        end_time = time.time()
+        print(f"Tempo di caricamento per {url}: {end_time - start_time} secondi")
+
+        with open(links_file, 'r', encoding='utf-8') as file:
             lines = file.readlines()
-        with open('ref.txt', 'w') as file:
+        with open(links_file, 'w', encoding='utf-8') as file:
             for line in lines:
                 if line.strip() == url:
                     file.write(f"#{line}")
                 else:
                     file.write(line)
-        continue
 
-    # Get the HTML content after elements are loaded
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
+def correct_json_formatting(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read().strip()
+        
+        if content.endswith(','):
+            content = content[:-1]
+        
+        if not content.startswith('['):
+            content = '[' + content
+        if not content.endswith(']'):
+            content = content + ']'
+        
+        products = json.loads(content)
+        
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump(products, file, ensure_ascii=False, indent=4)
+        
+        print(f"Formattazione del file JSON '{file_path}' corretta con successo.")
+    except json.JSONDecodeError as e:
+        print(f"Errore nella decodifica del JSON: {e}")
+    except Exception as e:
+        print(f"Errore durante la correzione del file JSON: {e}")
 
-    # Log time after loading page
-    load_page_time = time.time()
-    print(f"Time to load page and parse HTML from {url}: {load_page_time - start_time:.2f} seconds")
+if __name__ == "__main__":
+    output_file = 'prod.json'
+    links_file = 'ref.txt'
 
-    brand_element = soup.find('h5', class_='product-title__brand')
-    brand = brand_element.text.strip() if brand_element else 'Unknown'
+    with open(links_file, 'r') as file:
+        urls = [line.strip() for line in file.readlines() if not line.startswith('#')]
+    
+    options = Options()
+    options.headless = True
 
+    driver = webdriver.Remote(
+    command_executor='http://localhost:4444/wd/hub',
+    options=options
+    )
 
-    name_element = soup.find('h4', class_='product-title__name')
-    name = name_element.text.strip() if name_element else 'Unknown'
+    correct_json_formatting(output_file)
+    scrape_products(urls, driver, output_file, links_file)
+    correct_json_formatting(output_file)
 
-    # price_per_qty_element = soup.select_one('span.product__price-per-qty')
-    price_per_qty_element = soup.find('span', class_='product__price-per-qty')
-    price_per_qty = price_per_qty_element.text.strip() if price_per_qty_element else 'Unknown'
-
-    # price_element = soup.select_one('span.price__final')
-    price_element = soup.find('span', class_='price__final')
-    price = price_element.text.strip() if price_element else 'Unknown'
-
-    # Extract nutritional values
-    nutritional_values = {}
-    # nutrition_table = soup.select_one('div.product__attribute-table table.product-information-table')
-    nutrition_table = soup.find('table', class_='product-information-table')
-
-    if nutrition_table:
-        headers = [header.text.strip() for header in nutrition_table.select('thead th')]
-        rows = nutrition_table.select('tbody tr')
-        for row in rows:
-            columns = row.select('td')
-            if len(columns) >= len(headers):
-                nutrient = columns[0].text.strip()
-                values = {headers[i]: columns[i].text.strip() for i in range(1, len(headers))}
-                nutritional_values[nutrient] = values
-
-    print(f"Brand: {brand}")
-    print(f"Name: {name}")
-    print(f"Price per quantity: {price_per_qty}")
-    print(f"Price: {price}")
-    print(f"Nutritional values : {nutritional_values}")
-    print(f"Url: {url}")
-
-    # Open the file in append mode
-    with open('prod.txt', 'a') as file:
-
-        file.write(f"Brand: {brand}\n")
-        file.write(f"Name: {name}\n")
-        file.write(f"Price per quantity: {price_per_qty}\n")
-        file.write(f"Price: {price}\n")
-        file.write(f"Nutritional values: {nutritional_values}\n")
-        file.write(f"Url: {url}\n")
-        file.write('\n')
-
-    # Log del tempo dopo il caricamento della pagina
-    end_time = time.time()
-    print(f"Tempo di caricamento per {url}: {end_time - start_time} secondi")
-
-    # Se il caricamento è riuscito, aggiungi # all'inizio dell'URL in ref.txt
-    with open('ref.txt', 'r') as file:
+    # Rimuovi tutti gli # da ref.txt
+    with open(links_file, 'r', encoding='utf-8') as file:
         lines = file.readlines()
-    with open('ref.txt', 'w') as file:
+    with open(links_file, 'w', encoding='utf-8') as file:
         for line in lines:
-            if line.strip() == url:
-                file.write(f"#{line}")
-            else:
-                file.write(line)
+            file.write(line.lstrip('#'))
 
-# Chiudi il driver
-driver.quit()
+    driver.quit()
+
+
